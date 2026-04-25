379824bb6debcd6c281e71dc0b6552c3433f4a89
MareenaRoy <mareenaroyx26@gmail.com>
2026-04-25 21:33:28 +0530

    two folder added

---
+++ Mareena_phase3/functions/even_odd.js
@@ -0,0 +1,11 @@
+function even_odd(num){
+    if(num%2==0){
+        console.log("Number is even");
+    } else{
+        console.log("Number is odd");
+    }
+}
+
+even_odd(23);
+even_odd(44);
+even_odd(28);
